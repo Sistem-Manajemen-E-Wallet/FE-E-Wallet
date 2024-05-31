c75703561321442a1fc6843cda7319bd8f87e839
Berry Kurniawan <berykurniawan46@gmail.com>
2024-05-31 21:42:27 +0700

    feat: slicing product-list-page

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes as Router } from "react-router-dom";
 import Home from "../pages/Home";
 import Layout from "../pages/Layout";
+import Foodcourt from "../pages/Foodcourt";
 const Routes = () => {
   return (
     <Router>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="/product-list" element={<Foodcourt />}/>
       </Route>
     </Router>
   );
