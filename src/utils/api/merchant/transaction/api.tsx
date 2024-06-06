283ea77c8d3a5a272b7af474a8c3fc3f05f66570
Berry Kurniawan <berykurniawan46@gmail.com>
2024-06-06 15:39:49 +0700

    feat: merchant transaction

---
+++ src/utils/api/merchant/transaction/api.tsx
@@ -0,0 +1,57 @@
+import callAPI from "../../axiosWithConfig";
+
+interface Transaction {
+  TotalCost: number;
+}
+
+interface ApiResponse {
+  data: Transaction[];
+  meta: {
+    totalItems: number;
+  };
+}
+
+export async function getAllTransaction() {
+  try {
+    const ROOT_API = import.meta.env.VITE_REACT_API_URL;
+    const url = `${ROOT_API}/transactions`;
+    const response = await callAPI({
+      url,
+      method: "GET",
+      token: true,
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Failed to fetch transactions", error);
+    throw error;
+  }
+}
+
+export async function getTotalTrasnaction() {
+  try {
+    const data = await getAllTransaction();
+    const responseLength = data.meta.totalItems;
+    return responseLength;
+  } catch (error) {
+    console.error("Failed to get response length", error);
+    throw error;
+  }
+}
+
+export async function getTotalIncome(): Promise<number> {
+  try {
+    const data = await getAllTransaction();
+    let totalIncome: number = 0;
+
+    data.data.forEach((transaction: Transaction) => {
+      totalIncome += transaction.TotalCost;
+    });
+
+    console.log("Total Income:", totalIncome);
+
+    return totalIncome;
+  } catch (error) {
+    console.error("Failed to get total income", error);
+    throw error;
+  }
+}
