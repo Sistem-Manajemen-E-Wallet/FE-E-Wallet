024b49cb9211f1d09d52395caefdb50ef08036b2
kristianmikhael667 <kristianmikhael@gmail.com>
2024-06-10 11:01:05 +0700

    header dialog open close

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -79,7 +79,7 @@ const Register = () => {
   return (
     <section className="flex flex-col w-full h-screen justify-center bg-[#464BD8]">
       <div className="flex flex-col justify-center items-center container lg:px-96 md:px-64 sm:px-32 xs:px-24">
-        <div className="py-3 px-20 w-full bg-[#F3F6FF] rounded-xl">
+        <div className="py-3 px-5 w-full bg-[#F3F6FF] rounded-xl">
           <div className="flex justify-center">
             <img src={LogoIcon} className="flex justify-center mb-2" />
           </div>
@@ -93,7 +93,7 @@ const Register = () => {
               <div className="space-y-3">
                 <div className="mb-2">
                   <div className="flex flex-col space-y-1">
-                    <div className="flex flex-row gap-3 lg:gap-10">
+                    <div className="flex flex-row justify-center items-center gap-3">
                       <div
                         className={`flex-1 border-2  rounded-xl hover:border-[#464BD8] ${
                           role === "customer"
--- src/templates/Header.tsx
+++ src/templates/Header.tsx
@@ -1,20 +1,47 @@
 import { ModelLogout } from "@/components";
 import { useToken } from "@/utils/contexts/useToken";
 import { atom, useAtom } from "jotai";
+import { useEffect, useRef } from "react";
 import { Link, NavLink, useLocation } from "react-router-dom";
 import { LogoHistory, LogoHome, LogoLogout } from "../assets/logo";
 import Logo from "../assets/logo/logo.svg";
 
-const isOpenAtom = atom(false);
 const openLogoutAtom = atom(false);
+const isOpenAtom = atom(false);
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useAtom(isOpenAtom);
   const { user, changeToken } = useToken();
   const location = useLocation();
   const { pathname } = location;
   const splitLocation = pathname.split("/");
   const [openLogout, setOpenLogout] = useAtom(openLogoutAtom);
+
+  const [isSidebarOpen, setIsSidebarOpen] = useAtom(isOpenAtom);
+  const sidebarRef = useRef<HTMLDivElement>(null);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      sidebarRef.current &&
+      !sidebarRef.current.contains(event.target as Node)
+    ) {
+      setIsSidebarOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isSidebarOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isSidebarOpen]);
 
   const handleLogout = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
     e.preventDefault();
@@ -36,118 +63,117 @@ const Header = () => {
           </Link>
 
           <div className="flex justify-center">
-            <p
+            <button
+              type="button"
               className="text-xl mr-5 select-none"
-              onClick={() => setIsOpen(!isOpen)}
+              onClick={toggleSidebar}
             >
               Hi, <span className="font-bold">{user.name}</span>
-            </p>
-            {isOpen && (
-              <div
-                className={`z-10 absolute mt-12 ${
-                  isOpen ? "block" : "hidden"
-                } bg-white divide-y divide-gray-100 rounded-xl shadow-stone-950 w-auto`}
-              >
-                <ul className="">
-                  <NavLink
-                    to={"/"}
+            </button>
+
+            <div
+              ref={sidebarRef}
+              className={`z-10 absolute mt-12 ${
+                isSidebarOpen ? "block" : "hidden"
+              } bg-white divide-y divide-gray-100 rounded-xl shadow-stone-950 w-auto`}
+            >
+              <ul className="">
+                <NavLink
+                  to={"/"}
+                  className={`${
+                    splitLocation[1] === ""
+                      ? `text-primary-first`
+                      : "text-black"
+                  } flex px-4 py-2 hover:bg-gray-100`}
+                >
+                  <div
                     className={`${
                       splitLocation[1] === ""
-                        ? `text-primary-first`
-                        : "text-black"
-                    } flex px-4 py-2 hover:bg-gray-100`}
+                        ? `bg-primary-first`
+                        : `bg-secondary-first `
+                    } w-8 h-8 flex justify-center items-center rounded-full mr-2`}
                   >
-                    <div
-                      className={`${
-                        splitLocation[1] === ""
-                          ? `bg-primary-first`
-                          : `bg-secondary-first `
-                      } w-8 h-8 flex justify-center items-center rounded-full mr-2`}
-                    >
-                      <img src={LogoHome} alt="logohome" />
-                    </div>
-                    <p>Home</p>
-                  </NavLink>
-                  <NavLink
-                    to={"/history"}
+                    <img src={LogoHome} alt="logohome" />
+                  </div>
+                  <p>Home</p>
+                </NavLink>
+                <NavLink
+                  to={"/history"}
+                  className={`${
+                    splitLocation[1] === "history"
+                      ? `text-primary-first`
+                      : "text-black"
+                  } flex px-4 py-2 hover:bg-gray-100`}
+                >
+                  <div
                     className={`${
                       splitLocation[1] === "history"
-                        ? `text-primary-first`
-                        : "text-black"
-                    } flex px-4 py-2 hover:bg-gray-100`}
+                        ? `bg-primary-first`
+                        : `bg-secondary-first `
+                    } w-8 h-8 flex justify-center items-center rounded-full mr-2`}
                   >
-                    <div
-                      className={`${
-                        splitLocation[1] === "history"
-                          ? `bg-primary-first`
-                          : `bg-secondary-first `
-                      } w-8 h-8 flex justify-center items-center rounded-full mr-2`}
-                    >
-                      <img src={LogoHistory} alt="logohistory" />
-                    </div>
-                    <p>History</p>
-                  </NavLink>
-                  <NavLink
-                    to={"/profile"}
+                    <img src={LogoHistory} alt="logohistory" />
+                  </div>
+                  <p>History</p>
+                </NavLink>
+                <NavLink
+                  to={"/profile"}
+                  className={`${
+                    splitLocation[1] === "profile"
+                      ? `text-primary-first`
+                      : "text-black"
+                  } flex px-4 py-2 hover:bg-gray-100`}
+                >
+                  <div
                     className={`${
                       splitLocation[1] === "profile"
-                        ? `text-primary-first`
-                        : "text-black"
-                    } flex px-4 py-2 hover:bg-gray-100`}
+                        ? `bg-primary-first`
+                        : `bg-secondary-first `
+                    } w-8 h-8 flex justify-center items-center rounded-full mr-2`}
                   >
-                    <div
-                      className={`${
-                        splitLocation[1] === "profile"
-                          ? `bg-primary-first`
-                          : `bg-secondary-first `
-                      } w-8 h-8 flex justify-center items-center rounded-full mr-2`}
-                    >
-                      <img src={LogoHistory} alt="logohistory" />
+                    <img src={LogoHistory} alt="logohistory" />
+                  </div>
+                  <p>My Profile</p>
+                </NavLink>
+                <NavLink
+                  onClick={(e) => handleLogout(e)}
+                  className="flex px-4 py-2 hover:bg-gray-100"
+                  to="#"
+                >
+                  <div className="w-8 h-8 bg-secondary-first flex justify-center items-center rounded-full mr-2">
+                    <img src={LogoLogout} alt="logologout" />
+                  </div>
+                  <p>Logout</p>
+                </NavLink>
+                <ModelLogout
+                  open={openLogout}
+                  onClose={() => setOpenLogout(false)}
+                >
+                  <div className="text-center w-56">
+                    <h1 className="text-2xl text-red-700">Confirm Sign out</h1>
+                    <div className="mx-auto my-4 w-48">
+                      <p className="text-sm text-gray-500">
+                        Are you sure you want to sign out this account ?
+                      </p>
                     </div>
-                    <p>My Profile</p>
-                  </NavLink>
-                  <NavLink
-                    onClick={(e) => handleLogout(e)}
-                    className="flex px-4 py-2 hover:bg-gray-100"
-                    to="#"
-                  >
-                    <div className="w-8 h-8 bg-secondary-first flex justify-center items-center rounded-full mr-2">
-                      <img src={LogoLogout} alt="logologout" />
+                    <div className="flex gap-4">
+                      <button
+                        onClick={removeCookies}
+                        className="w-full px-1 py-1 rounded-lg bg-red-600 hover:bg-red-900"
+                      >
+                        Yes
+                      </button>
+                      <button
+                        onClick={() => setOpenLogout(false)}
+                        className="w-full px-1 py-1 rounded-lg bg-gray-500 hover:bg-gray-950"
+                      >
+                        Cancel
+                      </button>
                     </div>
-                    <p>Logout</p>
-                  </NavLink>
-                  <ModelLogout
-                    open={openLogout}
-                    onClose={() => setOpenLogout(false)}
-                  >
-                    <div className="text-center w-56">
-                      <h1 className="text-2xl text-red-700">
-                        Confirm Sign out
-                      </h1>
-                      <div className="mx-auto my-4 w-48">
-                        <p className="text-sm text-gray-500">
-                          Are you sure you want to sign out this account ?
-                        </p>
-                      </div>
-                      <div className="flex gap-4">
-                        <button
-                          onClick={removeCookies}
-                          className="w-full px-1 py-1 rounded-lg bg-red-600 hover:bg-red-900"
-                        >
-                          Yes
-                        </button>
-                        <button
-                          onClick={() => setOpenLogout(false)}
-                          className="w-full px-1 py-1 rounded-lg bg-gray-500 hover:bg-gray-950"
-                        >
-                          Cancel
-                        </button>
-                      </div>
-                    </div>
-                  </ModelLogout>
-                </ul>
-              </div>
-            )}
+                  </div>
+                </ModelLogout>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
